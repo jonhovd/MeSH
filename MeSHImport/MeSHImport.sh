035f41f2244d1c4dd9507a9cdbb3c4d3d340c7b6
Frode Roxrud Gill <frg@computas.com>
2015-12-08 13:28:28 +0100

    Prepare for 2016

--- MeSHImport/MeSHImport.sh
+++ MeSHImport/MeSHImport.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./MeSHImport localhost:9200 --clean --topnodes ./nordesc2015_topnodes.xml ./nordesc2015.xml
+./MeSHImport localhost:9200 --clean --topnodes ./nordesc_topnodes.xml ./nordesc2016.xml
