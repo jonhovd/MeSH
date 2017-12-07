a8cc6da76c0620afd401c6d448ff139b2836ac4b
Frode Roxrud Gill <frodegill@gmail.com>
2017-12-07 14:18:29 +0000

    Nordesc2018

--- MeSHImport/MeSHImport.sh
+++ MeSHImport/MeSHImport.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./MeSHImport localhost:9200 --clean --topnodes ./nordesc_topnodes.xml ./nordesc2017.xml
+./MeSHImport localhost:9200 --clean --topnodes ./nordesc_topnodes.xml ./nordesc2018.xml
