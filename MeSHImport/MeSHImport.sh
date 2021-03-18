0f88652c8ad5f91f9ee9fffc63e8794cb9eece6f
Frode Roxrud Gill <frodegill@gmail.com>
2021-03-18 11:44:00 +0100

    Upgraded to nordesc2019

--- MeSHImport/MeSHImport.sh
+++ MeSHImport/MeSHImport.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./MeSHImport localhost:9200 --clean --topnodes ./nordesc_topnodes.xml ./nordesc2018.xml
+./MeSHImport localhost:9200 --clean --topnodes ./nordesc_topnodes.xml ./nordesc2019.xml
