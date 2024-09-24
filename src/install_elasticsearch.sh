7dbfa8d66d869bd50e8be13acd5f5697a43f22c9
jonhovd <jonhovd@outlook.com>
2024-09-24 14:44:31 +0200

    Flyttet skriptene til hoved prosjekt mappen

--- src/install_elasticsearch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo apt-get -y update &&
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add - &&
-sudo apt-get -y install apt-transport-https &&
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-7.x.list
-sudo apt-get -y update && sudo apt-get -y install elasticsearch &&
-sudo /bin/systemctl daemon-reload &&
-sudo /bin/systemctl enable elasticsearch.service
